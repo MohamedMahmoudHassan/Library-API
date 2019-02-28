a193f76a6710d42c3799307d9a84f74f5c99957d
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-02-28 04:49:58 +0200

    Books filtering - fetal error handled

--- model/books.js
+++ model/books.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+// eslint-disable-next-line no-unused-vars
+const startupDebugger = require('debug')('app:startup');
 
 function validation(body) {
   const Schema = {
     name: Joi.string().min(3).max(20).required(),
     author: Joi.string().min(5).max(20),
     description: Joi.string().min(40).max(200),
-    pages_no: Joi.number(),
+    pages_no: Joi.number().required(),
     img_loc: Joi.string(),
     cpy_loc: Joi.string().required(),
     ebook_price: Joi.number().required(),
@@ -19,7 +21,7 @@ const Book = mongoose.model('Book', new mongoose.Schema({
   name: { type: String, required: true },
   author: { type: String },
   des: { type: String },
-  pages_no: { type: Number },
+  pages_no: { type: Number, required: true },
   img_loc: { type: String },
   cpy_loc: { type: String, required: true },
   ebook_price: { type: Number, required: true },
@@ -27,9 +29,14 @@ const Book = mongoose.model('Book', new mongoose.Schema({
 }));
 
 function handleQuery(query) {
-  const sortSplit = query.sort.split('_');
-  let sortFilter;
-  sortFilter[sortSplit[0]] = sortFilter[1] === 'asc' ? 1 : -1;
+  let sortSplit;
+  let sortFilter = {};
+  if (query.sort) {
+    sortSplit = query.sort.split('_');
+    sortFilter[sortSplit[0]] = sortFilter[1] === 'dsc' ? -1 : 1;
+  } else {
+    sortFilter = { name: 1 };
+  }
 
   const lim = query.max || 8;
   const page = query.page || 1;
@@ -37,11 +44,11 @@ function handleQuery(query) {
   return {
     find: [
       { name: query.name || /.*.*/ },
-      { ebook_price: { lte: query.eMaxPrice || 100000, gte: query.eMinPrice || 1 } },
-      { hard_price: { lte: query.hMaxPrice || 100000, gte: query.hMinPrice || 1 } },
-      { pages_no: { lte: query.maxPages || 100000, gte: query.minPages || 1 } },
+      { ebook_price: { $lte: query.eMaxPrice || 100000, $gte: query.eMinPrice || 1 } },
+      { hard_price: { $lte: query.hMaxPrice || 100000, $gte: query.hMinPrice || 1 } },
+      { pages_no: { $lte: query.maxPages || 100000, $gte: query.minPages || 1 } },
     ],
-    sort: { sortFilter },
+    sort: sortFilter,
     limit: lim,
     skip: (page - 1) * lim,
   };
