eb6414b2099a89b44e5c3c131b6bcbad1465c959
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-01 21:29:43 +0200

    Login with only validation

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const startupDebugger = require('debug')('app:startup');
 const winston = require('winston');
+const { loginValidation, User } = require('../model/users');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Enter your login data here.');
+});
+
+// eslint-disable-next-line consistent-return
+router.post('/', async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Invalid email or password');
+
+  const valPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!valPassword) return res.status(400).send('Invalid email or password');
+
+  res.send(user);
 });
 
 
