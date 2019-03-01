239b9fad85b75aece33a80a27462afcd0d9bf398
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-01 23:49:48 +0200

    Login - generate gwt with n(n=7) days

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-underscore-dangle */
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const startupDebugger = require('debug')('app:startup');
 const winston = require('winston');
 const { loginValidation, User } = require('../model/users');
@@ -9,6 +11,10 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.send('Enter your login data here.');
 });
+
+function dateAfter(time, days) {
+  return time + days * 8640000;
+}
 
 // eslint-disable-next-line consistent-return
 router.post('/', async (req, res) => {
@@ -21,7 +27,8 @@ router.post('/', async (req, res) => {
   const valPassword = await bcrypt.compare(req.body.password, user.password);
   if (!valPassword) return res.status(400).send('Invalid email or password');
 
-  res.send(user);
+  const token = jwt.sign({ id: user._id, type: user.type, logoutDate: dateAfter(Date.now(), 7) }, 'someWord');
+  res.send(token);
 });
 
 
