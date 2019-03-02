dd74c38f4a18924f996469eba9b3b37e2e174ee9
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-02 05:42:18 +0200

    customers model

--- model/cusomers.js
+++ model/cusomers.js
@@ -3,6 +3,17 @@ const Joi = require('joi');
 
 Joi.objectId = require('joi-objectid')(Joi);
 
+function validate(body) {
+  const Schema = {
+    user_id: Joi.objectId().required(),
+    cart: Joi.array().items(Joi.string()),
+    bor_list: Joi.array().items(Joi.string()),
+    ebooks_list: Joi.array().items(Joi.string()),
+    bought_list: Joi.array().items(Joi.string()),
+  };
+  return Joi.validate(body, Schema);
+}
+
 const Customer = mongoose.model('Customer', new mongoose.Schema({
   user_id: { type: String, required: true },
   cart: [{ type: String }],
@@ -12,3 +23,4 @@ const Customer = mongoose.model('Customer', new mongoose.Schema({
 }));
 
 module.exports.Customer = Customer;
+module.exports.validate = validate;
