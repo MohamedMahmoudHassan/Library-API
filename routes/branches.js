79499c7061d948bc14cb5393cd5105e4a7516822
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-03 22:03:06 +0200

    info about each branch (just clerks)

--- routes/branches.js
+++ routes/branches.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { Branch } = require('../model/branches');
+const { User } = require('../model/clerks');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const branches = await Branch.find();
   res.send(branches);
+});
+
+router.get('/:id', async (req, res) => {
+  const branch = await Branch.findOne({ _id: req.params.id });
+  const branchClerks = await User.findOne({ branch_id: req.params.id }).select('user_id -_id');
+  res.send({ branch, clerks: branchClerks });
 });
 
 module.exports = router;
