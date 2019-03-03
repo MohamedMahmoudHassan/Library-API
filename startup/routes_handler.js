07d64f73debba0c687ebb94e5ed50db541363c46
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-03 21:50:11 +0200

    Library braches list

--- startup/routes_handler.js
+++ startup/routes_handler.js
@@ -2,13 +2,14 @@ const express = require('express');
 const home = require('../routes/home');
 const login = require('../routes/login');
 const reg = require('../routes/registration');
-const books = require('../routes/books');
 const profile = require('../routes/profile');
+const books = require('../routes/books');
 const addBook = require('../routes/add_book');
 const editBook = require('../routes/edit_book');
 const usersList = require('../routes/users_list');
-const addBranch = require('../routes/add_branch');
 const addCopies = require('../routes/add_copies');
+const branches = require('../routes/branches');
+const addBranch = require('../routes/add_branch');
 
 module.exports = (app) => {
   app.use(express.json());
@@ -16,11 +17,12 @@ module.exports = (app) => {
   app.use('/home', home);
   app.use('/login', login);
   app.use('/reg', reg);
-  app.use('/books_list', books);
   app.use('/profile', profile);
+  app.use('/books', books);
   app.use('/add_book', addBook);
   app.use('/edit_book', editBook);
   app.use('/users_list', usersList);
-  app.use('/add_branch', addBranch);
   app.use('/add_copies', addCopies);
+  app.use('/branches', branches);
+  app.use('/add_branch', addBranch);
 };
