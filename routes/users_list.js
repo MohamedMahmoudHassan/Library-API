b81b74b9677dd9fa504f98cb21a1c55c6e56adaa
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-05 09:02:48 +0200

    customers list

--- routes/users_list.js
+++ routes/users_list.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { User } = require('../model/users');
+const customers = require('../model/customers');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const users = await User.find();
   res.send(users);
+});
+
+router.get('/customers', async (req, res) => {
+  const customersList = await customers.User.find().populate('user_id');
+  res.send(customersList);
 });
 
 module.exports = router;
