fbc5f54c3006e66caa0a1f0a5f533427496e8e72
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-06 02:33:11 +0200

    Customer profile -> details about each request in cart

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { User } = require('../model/customers');
+const { BuyRecord } = require('../model/buy_records');
 
 const router = express.Router();
 
@@ -9,5 +10,11 @@ router.get('/', async (req, res) => {
     .populate({ path: 'user_id', select: '-password -_id -__v -type' });
   res.send(user);
 });
+
+router.get('/myCart/:id', async (req, res) => {
+  const record = await BuyRecord.find({ _id: req.params.id });
+  res.send(record);
+});
+
 
 module.exports = router;
