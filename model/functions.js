b67d4fb4dbee8b02bedee2103512041be771259e
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-06 05:01:12 +0200

    function check if id is valid

--- model/functions.js
+++ model/functions.js
@@ -1,5 +1,16 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
 function validationErr(msg) {
   return { details: [{ message: msg }] };
 }
 
+function valId(body) {
+  const Schema = {
+    id: Joi.objectId(),
+  };
+  return Joi.validate(body, Schema);
+}
+
 module.exports.validationErr = validationErr;
+module.exports.valId = valId;
