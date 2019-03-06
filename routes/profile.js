274c24a35e9742a19d4f435057fd2be868bf42d1
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-06 03:26:40 +0200

    delete item from cart

--- routes/profile.js
+++ routes/profile.js
@@ -11,10 +11,24 @@ router.get('/', async (req, res) => {
   res.send(user);
 });
 
+
 router.get('/myCart/:id', async (req, res) => {
   const record = await BuyRecord.find({ _id: req.params.id });
   res.send(record);
 });
 
+router.delete('/deleteFromCart/:id', async (req, res) => {
+  await BuyRecord.findOneAndUpdate({ _id: req.params.id }, {
+    $set: { status: -1 },
+  });
+
+  const cart = await User.findOneAndUpdate(
+    { user_id: req.headers.user_id },
+    { $pull: { cart: req.params.id } },
+    { new: true },
+  );
+
+  res.send(cart);
+});
 
 module.exports = router;
