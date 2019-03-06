df62fa739f7a9a241b046737a5af7cd708d04e80
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-06 02:16:44 +0200

    Customer profile -> user data, cart and requestes

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const { User } = require('../model/customers');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Your data is here...Isn\'t ?');
+router.get('/', async (req, res) => {
+  const user = await User
+    .find({ user_id: req.headers.user_id })
+    .populate({ path: 'user_id', select: '-password -_id -__v -type' });
+  res.send(user);
 });
 
 module.exports = router;
