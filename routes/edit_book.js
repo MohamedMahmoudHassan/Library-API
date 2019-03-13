ce785f3f99abc31d8f1d16fb2d105a6bd4afccc8
MohamedMahmoudHassan <mohamedmahmoudalihassan@gmail.com>
2019-03-13 21:35:11 +0200

    books edit -findOne insted of findById- - better id validation before delete

--- routes/edit_book.js
+++ routes/edit_book.js
@@ -3,6 +3,7 @@ const express = require('express');
 // eslint-disable-next-line no-unused-vars
 const debug = require('debug')('app:startup');
 const { Book, editValidate } = require('../model/books');
+const { isValId } = require('../model/functions');
 
 const router = express.Router();
 
@@ -16,19 +17,19 @@ router.put('/:id', async (req, res) => {
   const { error } = editValidate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const book = await Book.findByIdAndUpdate(req.params.id, { $set: req.body });
+  const book = await Book.findOneAndUpdate(
+    { _id: req.params.id }, { $set: req.body }, { new: true },
+  );
   if (!book) return res.status(400).send('No book to update.');
 
   res.send(book);
 });
 
 router.delete('/:id', async (req, res) => {
-  req.body.id = req.params.id;
-
-  const { error } = editValidate(req.body);
+  const { error } = isValId({ id: req.params.id });
   if (error) return res.status(400).send(error.details[0].message);
 
-  const book = await Book.findByIdAndDelete(req.params.id);
+  const book = await Book.findOneAndDelete({ _id: req.params.id });
   if (!book) return res.status(400).send('No book to delete.');
 
   res.send(book);
